9f13aa90ca0dbf5acf53feebbe6c643d59427751
Flávia Reis <flaviarster@gmail.com>
2024-03-07 14:08:08 -0300

    Add font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Libre': ['Libre Franklin', 'sans-serif'],
+      },
+      colors: {
+        'blue': 'hsl(223, 87%, 63%)',
+        'pale-blue': 'hsl(223, 100%, 88%)',
+        'light-red': 'hsl(354, 100%, 66%)',
+        'gray': 'hsl(0, 0%, 59%)',
+      },
+    },
   },
   plugins: [],
 }
